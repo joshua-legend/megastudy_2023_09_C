c396a6c1d4a27a6325fbd16e108dc8b1cb5c1b3e
Joshua <76941328+joshua-legend@users.noreply.github.com>
2023-09-15 20:37:09 +0900

    C언어 선생 바뀜 첫 자료

---
+++ ch06/ex01.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a;
+	printf("입력:");
+	scanf_s("%d", &a);
+
+
+	int ten_thousand;
+	int thousand;
+	int hundred;
+	int ten;
+	int one;
+	int divider = 10000;
+
+	ten_thousand = a / divider;
+	a = a % divider;
+	divider = divider / 10;
+	
+	thousand = a / divider;
+	a = a % divider;
+	divider = divider / 10;
+
+	hundred = a / divider;
+	a = a % divider;
+	divider = divider / 10;
+
+	ten = a / divider;
+	a = a % divider;
+	divider = divider / 10;
+
+	one = a / divider;
+	a = a % divider;
+	divider = divider / 10;
+
+	printf("%d만%d천%d백%d십%d", ten_thousand, thousand, hundred, ten, one);
+
+
+}
